37d10132713114b78f63a7ebec9d1737c09cbc2c
gcajay95 <gcajay95@gmail.com>
2024-05-12 11:52:04 -0700

    Getting Images from S3

--- src/components/ItemCard/ItemData.js
+++ src/components/ItemCard/ItemData.js
@@ -3,28 +3,30 @@ export const itemData = [
   {
     id: 1,
     name: "Cup",
-    image: require("../../images/cup.jpeg"),
+    image:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/bag_with_belt.jpeg",
     price: 100,
     description: "This is a beautiful cup.",
   },
   {
     id: 2,
     name: "Stylish Bag",
-    image: require("../../images/bag.jpeg"),
+    image: "https://boozercart.s3.us-west-1.amazonaws.com/media/bag.jpeg",
     price: 200,
     description: "This is a stylish bag.",
   },
   {
     id: 3,
     name: "Trendy Bag",
-    image: require("../../images/bag2.jpeg"),
+    image: "https://boozercart.s3.us-west-1.amazonaws.com/media/bag2.jpeg",
     price: 200,
     description: "This is a trendy bag.",
   },
   {
     id: 4,
     name: "Bag with Belt",
-    image: require("../../images/bag_with_belt.jpeg"),
+    image:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/bag_with_belt.jpeg",
     price: 200,
     description: "This is a bag with a belt.",
   },
