7599db584a42cecced8a112d490140ddff643774
gcajay95 <gcajay95@gmail.com>
2024-05-12 12:11:30 -0700

    Extra Items

--- src/components/ItemCard/ItemData.js
+++ src/components/ItemCard/ItemData.js
@@ -2,32 +2,61 @@
 export const itemData = [
   {
     id: 1,
-    name: "Cup",
+    name: "Leather Bag",
     image:
       "https://boozercart.s3.us-west-1.amazonaws.com/media/bag_with_belt.jpeg",
-    price: 100,
+    price: 99.99,
     description: "This is a beautiful cup.",
   },
   {
     id: 2,
     name: "Stylish Bag",
     image: "https://boozercart.s3.us-west-1.amazonaws.com/media/bag.jpeg",
-    price: 200,
+    price: 144.99,
     description: "This is a stylish bag.",
   },
   {
     id: 3,
     name: "Trendy Bag",
     image: "https://boozercart.s3.us-west-1.amazonaws.com/media/bag2.jpeg",
-    price: 200,
+    price: 109.89,
     description: "This is a trendy bag.",
   },
   {
     id: 4,
-    name: "Bag with Belt",
+    name: "Party Shoes",
     image:
-      "https://boozercart.s3.us-west-1.amazonaws.com/media/bag_with_belt.jpeg",
-    price: 200,
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/shoes_brown.jpeg",
+    price: 119,
+    description: "This is a bag with a belt.",
+  },
+  {
+    id: 5,
+    name: "Office Bag",
+    image: "https://boozercart.s3.us-west-1.amazonaws.com/media/bag_cup.jpeg",
+    price: 119.99,
+    description: "This is a bag with a belt.",
+  },
+  {
+    id: 6,
+    name: "Cup Set",
+    image: "https://boozercart.s3.us-west-1.amazonaws.com/media/cup.jpeg",
+    price: 19.99,
+    description: "This is a bag with a belt.",
+  },
+  {
+    id: 7,
+    name: "Coffe Cup",
+    image: "https://boozercart.s3.us-west-1.amazonaws.com/media/cup-2.jpeg",
+    price: 9.99,
+    description: "This is a bag with a belt.",
+  },
+  {
+    id: 8,
+    name: "Cup for Cat",
+    image:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/bag_cup_cat.jpeg",
+    price: 19.99,
     description: "This is a bag with a belt.",
   },
 ];
