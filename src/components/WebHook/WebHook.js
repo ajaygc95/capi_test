150223d76687922343ce470129086d0500415618
Ajay GC <ajaygc80@meta.com>
2024-06-27 15:07:40 -0700

    WebHooks Added

---
+++ src/components/WebHook/WebHook.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainContainer, ProductTitle } from "./WebHook.element";
+
+function WebHook() {
+  return (
+    <MainContainer>
+      <ProductTitle>This is the webHooks page</ProductTitle>
+    </MainContainer>
+  );
+}
+
+export default WebHook;
