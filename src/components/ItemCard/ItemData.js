90df56cdfa9c9a42ef37dc947ef1a559525f882e
gcajay95 <gcajay95@gmail.com>
2024-07-19 22:52:37 -0700

    Image from S3

--- src/components/ItemCard/ItemData.js
+++ src/components/ItemCard/ItemData.js
@@ -1,69 +1,67 @@
 // src/components/itemcard/itemdata.js
 
-import bag from "../../images/bag.jpeg";
-import shihtzucup from "../../images/shihtzu_mug.jpeg";
-import shihtzu_on_cup_pink from "../../images/shihtzu_on_cup_pink.jpeg";
-import shihtzu_on_sweatshirt from "../../images/sweatshirt_shihtzu.jpeg";
-import shih_tzu_picture_on_pillow from "../../images/shih_tzu_picture_on_pillow.jpeg";
-import shih_tzu_coffee_mug from "../../images/shihtzu_plastic_coffee_mug.jpeg";
-import watter_bottle from "../../images/watter_bottle.jpeg";
-import phone_case from "../../images/phone_case.jpeg";
-import plush_doll from "../../images/plush_doll.jpg";
-
 export const itemData = [
   {
     id: 1,
     name: "Plush Doll",
-    image_url: plush_doll,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/plush_doll.jpg",
     price: 24.99,
     description: "A cute and soft plush doll perfect for cuddles.",
   },
   {
     id: 2,
     name: "Shihtzu Mug",
-    image_url: shihtzucup,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/shihtzu_mug.jpeg",
     price: 14.99,
     description: "A mug featuring a Shihtzu dog design.",
   },
   {
     id: 3,
     name: "Shihtzu on Pink Cup",
-    image_url: shihtzu_on_cup_pink,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/shihtzu_on_cup_pink.jpeg",
     price: 16.99,
     description: "A pink cup adorned with a Shihtzu dog.",
   },
   {
     id: 4,
     name: "Shihtzu Sweatshirt",
-    image_url: shihtzu_on_sweatshirt,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/sweatshirt_shihtzu.jpeg",
     price: 39.99,
     description: "A cozy sweatshirt featuring a Shihtzu dog graphic.",
   },
   {
     id: 5,
     name: "Shihtzu Picture on Pillow",
-    image_url: shih_tzu_picture_on_pillow,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/shih_tzu_picture_on_pillow.jpeg",
     price: 29.99,
     description: "A decorative pillow with a Shihtzu dog print.",
   },
   {
     id: 6,
     name: "Shihtzu Coffee Mug",
-    image_url: shih_tzu_coffee_mug,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/shihtzu_plastic_coffee_mug.jpeg",
     price: 12.99,
     description: "A durable coffee mug with a Shihtzu dog.",
   },
   {
     id: 7,
     name: "Water Bottle",
-    image_url: watter_bottle,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/watter_bottle.jpeg",
     price: 17.99,
     description: "A stylish water bottle to keep you hydrated.",
   },
   {
     id: 8,
     name: "Phone Case",
-    image_url: phone_case,
+    image_url:
+      "https://boozercart.s3.us-west-1.amazonaws.com/media/phone_case.jpeg",
     price: 21.99,
     description: "A protective phone case with a unique design.",
   },
